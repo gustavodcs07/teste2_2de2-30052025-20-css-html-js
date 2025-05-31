95e258a3ef01106b4b75618cdbf25ef47c2465a0
Gustavo de C. Silva <gustavodecarvalhosilva07@gmail.com>
2025-05-30 21:36:05 -0300

    Add files via upload

--- TESTE2-2de2/js/script.js
+++ TESTE2-2de2/js/script.js
@@ -2,9 +2,9 @@ function mostrarIdade(ano,idade){
     ano = new Date().getFullYear();
 console.log(ano);
 var nome = document.getElementById('nome').value;
- var idade = document.getElementById('idade').value;
- document.getElementById('mensagem').textContent = `Olá ${nome}! Você
- tem ${ano - idade} anos`;
+var idade = document.getElementById('idade').value;
+document.getElementById('mensagem').textContent = `Olá ${nome}! Você
+tem ${ano - idade} anos`;
 }
 
 
